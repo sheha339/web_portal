873a7a136ec7953fae37ae8aecd6968fa33f2f06
sheha339 <30966059+sheha339@users.noreply.github.com>
2021-07-27 02:00:16 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php 	
 	
 	$host 		= "localhost";
-	$user		= "sheha";
-	$pass		= "Sheha339";
+	$user		= "root";
+	$pass		= "";
 	$database 	= "db_berita";
 
     $db = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
